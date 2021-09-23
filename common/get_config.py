bfd3cdf0d298bc5ec92342efda4022d5e3c2f383
娃蛋 <119471432@qq.com>
2021-09-23 23:53:28 +0800

    test

--- common/get_config.py
+++ common/get_config.py
@@ -6,7 +6,7 @@ from common.get_path import CONFIG_PATH
 def get_config(file_path):
     config = ConfigParser()
     config.read(file_path)
-    return config
+    return config 
 
 
 config_data = get_config(os.path.join(CONFIG_PATH, "config.ini"))
